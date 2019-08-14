0e2007dc7b63958003adb6658c4794059f169cc8
weiyaoxi <weiyaoxi@sina.com>
2019-08-14 11:06:26 +0800

    优化webservice初始化的时候拉取wsdl加快速度

--- Source/Wdxx.Core/CoreClient.cs
+++ Source/Wdxx.Core/CoreClient.cs
@@ -23,103 +23,27 @@ namespace Wdxx.Core
         public string ServiceUrl { get; set; }
 
         /// <summary>
+        /// webservice类
+        /// </summary>
+        private static Type _type;
+
+        /// <summary>
         /// 带服务地址构造函数
         /// </summary>
         /// <param name="serviceUrl">服务地址</param>
         public CoreClient(string serviceUrl)
         {
             ServiceUrl = serviceUrl.TrimEnd('/');
-        }
-
-        /// <summary>
-        /// 发送请求(返回字符串)
-        /// </summary>
-        /// <param name="method">请求的方法</param>
-        /// <param name="sendData">请求参数 (可变参数与服务端参数一致)</param>
-        /// <returns></returns>
-        public string Send(string method, params object[] sendData)
-        {
-            return SendCore(method, sendData);
-        }
-
-        /// <summary>
-        /// 发送请求(返回有构造函数的类)
-        /// </summary>
-        /// <param name="method">请求的方法</param>
-        /// <param name="sendData">请求参数 (可变参数与服务端参数一致)</param>
-        /// <returns></returns>
-        public T Send<T>(string method, params object[] sendData) where T : new()
-        {
-            var ret = SendCore(method, sendData);
-            return (T)CoreConvert.JsonDataToObj(ret, typeof(T));
-        }
-
-        /// <summary>
-        /// 发送请求(核心)
-        /// </summary>
-        /// <param name="method">请求的方法</param>
-        /// <param name="sendData">请求参数 (可变参数与服务端参数一致)</param>
-        /// <returns></returns>
-        private string SendCore(string method, params object[] sendData)
-        {
-            try
-            {
-                if (!string.IsNullOrEmpty(CoreHttp.Get(ServiceUrl + "?WSDL")))
-                {
-                    var ret = InvokeWebService(ServiceUrl, method, sendData);
-                    return ret == null ? null : CoreConvert.ObjToJsonData(ret);
-                }
-                string result;
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create(ServiceUrl + "/WebSrviceSoap");
-                httpWebRequest.Method = "POST";
-                httpWebRequest.ContentType = "text;charset=UTF-8";
-                //这个在非GET的时候，一定要加上，如果服务器返回错误，他还会继续再去请求，不会使用之前的错误数据做返回数据
-                httpWebRequest.ServicePoint.Expect100Continue = false;
-                var datas = sendData == null || sendData.Length == 0 ? string.Empty : CoreConvert.ObjToJsonData(sendData.Select(CoreConvert.ObjToJsonData).ToList());
-                var tmpData = new SendData { Method = method, Datas = datas };
-                var data = Encoding.UTF8.GetBytes(CoreConvert.ObjToJsonData(tmpData));
-                httpWebRequest.ContentLength = data.Length;
-                var outStream = httpWebRequest.GetRequestStream();
-                outStream.Write(data, 0, data.Length);
-                outStream.Close();
-                var webResponse = httpWebRequest.GetResponse();
-                var httpWebResponse = (HttpWebResponse)webResponse;
-                var stream = httpWebResponse.GetResponseStream();
-                if (stream != null)
-                {
-                    var streamReader = new StreamReader(stream, Encoding.GetEncoding("UTF-8"));
-                    result = streamReader.ReadToEnd();
-                    streamReader.Close();
-                    stream.Close();
-                }
-                else
-                {
-                    result = string.Empty;
-                }
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("HttpErr" + " uri:" + ServiceUrl + " method:" + method + " httpData:" + sendData + "err:" + ex);
-            }
-        }
-
-        /// <summary>
-        /// 调用WebServices
-        /// </summary>
-        /// <param name="url">WebServices地址</param>
-        /// <param name="method">调用的方法</param>
-        /// <param name="sendData">把webservices里需要的参数按顺序放到这个object[]里</param>
-        public static object InvokeWebService(string url, string method, params object[] sendData)
-        {
             try
             {
                 //获取WSDL
                 var wc = new WebClient();
-                var stream = wc.OpenRead(url + "?WSDL");
+                var stream = wc.OpenRead(ServiceUrl + "?WSDL");
+                wc.Dispose();
                 if (stream == null)
                 {
-                    return null;
+                    _type = null;
+                    return;
                 }
                 var sd = ServiceDescription.Read(stream);
                 var classname = sd.Services[0].Name;
@@ -157,33 +81,110 @@ namespace Wdxx.Core
                 }
                 //生成代理实例，并调用方法
                 var assembly = cr.CompiledAssembly;
-                var t = assembly.GetType(classname, true, true);
-                var obj = Activator.CreateInstance(t);
-                var mi = t.GetMethod(method);
-                //方法不存在直接返回null
-                if (mi == null)
-                {
-                    return null;
-                }
-                //暂存的xml参数组
-                var sendDataXml = sendData.Select(CoreConvert.ObjToJsonData).ToList();
-                //具体的参数数组声明
-                var objArr = new object[sendDataXml.Count];
-                //获取参数组
-                var ps = mi.GetParameters();
-                for (var i = 0; i < ps.Length; i++)
-                {
-                    //根据参数组中的类型反序列化成需要的类型
-                    objArr[i] = CoreConvert.JsonDataToObj(sendDataXml[i], ps[i].ParameterType);
-                }
-                //执行方法
-                return mi.Invoke(obj, objArr);
+                _type = assembly.GetType(classname, true, true);
             }
             catch (Exception e)
             {
                 CoreLog.Error(e);
+            }
+        }
+
+        /// <summary>
+        /// 发送请求(返回字符串)
+        /// </summary>
+        /// <param name="method">请求的方法</param>
+        /// <param name="sendData">请求参数 (可变参数与服务端参数一致)</param>
+        /// <returns></returns>
+        public string Send(string method, params object[] sendData)
+        {
+            return SendCore(method, sendData);
+        }
+
+        /// <summary>
+        /// 发送请求(返回有构造函数的类)
+        /// </summary>
+        /// <param name="method">请求的方法</param>
+        /// <param name="sendData">请求参数 (可变参数与服务端参数一致)</param>
+        /// <returns></returns>
+        public T Send<T>(string method, params object[] sendData) where T : new()
+        {
+            var ret = SendCore(method, sendData);
+            return (T)CoreConvert.JsonDataToObj(ret, typeof(T));
+        }
+
+        /// <summary>
+        /// 发送请求(核心)
+        /// </summary>
+        /// <param name="method">请求的方法</param>
+        /// <param name="sendData">请求参数 (可变参数与服务端参数一致)</param>
+        /// <returns></returns>
+        private string SendCore(string method, params object[] sendData)
+        {
+            try
+            {
+                //若存在webservice类 执行反射调用
+                if (_type != null)
+                {
+                    var ret = Fun(method, sendData);
+                    return ret == null ? null : CoreConvert.ObjToJsonData(ret);
+                }
+                string result;
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(ServiceUrl + "/WebSrviceSoap");
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "text;charset=UTF-8";
+                //这个在非GET的时候，一定要加上，如果服务器返回错误，他还会继续再去请求，不会使用之前的错误数据做返回数据
+                httpWebRequest.ServicePoint.Expect100Continue = false;
+                var datas = sendData == null || sendData.Length == 0 ? string.Empty : CoreConvert.ObjToJsonData(sendData.Select(CoreConvert.ObjToJsonData).ToList());
+                var tmpData = new SendData { Method = method, Datas = datas };
+                var data = Encoding.UTF8.GetBytes(CoreConvert.ObjToJsonData(tmpData));
+                httpWebRequest.ContentLength = data.Length;
+                var outStream = httpWebRequest.GetRequestStream();
+                outStream.Write(data, 0, data.Length);
+                outStream.Close();
+                var webResponse = httpWebRequest.GetResponse();
+                var httpWebResponse = (HttpWebResponse)webResponse;
+                var stream = httpWebResponse.GetResponseStream();
+                if (stream != null)
+                {
+                    var streamReader = new StreamReader(stream, Encoding.GetEncoding("UTF-8"));
+                    result = streamReader.ReadToEnd();
+                    streamReader.Close();
+                    stream.Close();
+                }
+                else
+                {
+                    result = string.Empty;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("HttpErr" + " uri:" + ServiceUrl + " method:" + method + " httpData:" + sendData + "err:" + ex);
+            }
+        }
+
+        private static object Fun(string method, params object[] sendData)
+        {
+            var mi = _type.GetMethod(method);
+            //方法不存在直接返回null
+            if (mi == null)
+            {
                 return null;
             }
+            //暂存的xml参数组
+            var sendDataXml = sendData.Select(CoreConvert.ObjToJsonData).ToList();
+            //具体的参数数组声明
+            var objArr = new object[sendDataXml.Count];
+            //获取参数组
+            var ps = mi.GetParameters();
+            for (var i = 0; i < ps.Length; i++)
+            {
+                //根据参数组中的类型反序列化成需要的类型
+                objArr[i] = CoreConvert.JsonDataToObj(sendDataXml[i], ps[i].ParameterType);
+            }
+            var obj = Activator.CreateInstance(_type);
+            //执行方法
+            return mi.Invoke(obj, objArr);
         }
     }
 }
